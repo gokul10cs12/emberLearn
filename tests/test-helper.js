02c9e50e5012332ea12e2673f82361c689c9e5dd
Gokul N B <gokulnb@MacBook-Pro.local>
2021-09-19 20:21:09 +0530

    first commit

---
+++ tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'ember-learn/app';
+import config from 'ember-learn/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
