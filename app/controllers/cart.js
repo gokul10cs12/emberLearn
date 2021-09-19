ed00bd91f926a1ead902044df82fa57d7434f7c4
gokulnb <gokul10cs12@gmail.com>
2021-09-20 01:43:22 +0530

    file addition

---
+++ app/controllers/cart.js
@@ -0,0 +1,22 @@
+import Controller from '@ember/controller';
+
+export default class CartController extends Controller {
+    // subtotal = 12;
+    // tax = 15;
+    // total = 0;
+
+    get tax(){
+        return 0.09 * this.subtotal;
+    }
+
+    get total(){
+        return this.subtotal + this.tax; 
+    }
+
+    get subtotal(){
+        return this.model.reduce((acc, item) => {
+            console.log("a=",acc, "b=", item);
+            return acc + item.price;
+        }, 0);
+    }
+}
