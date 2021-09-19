36952e23904344a0d9403af7d6bcb8233965e49a
gokulnb <gokul10cs12@gmail.com>
2021-09-20 01:30:07 +0530

    controller and router

--- app/routes/item.js
+++ app/routes/item.js
@@ -3,7 +3,6 @@ import Route from '@ember/routing/route';
 export default class ItemRoute extends Route {
   model(param) {
     const { item_id } = param;
-    console.log('testing', item_id);
     return item_id;
   }
 }
