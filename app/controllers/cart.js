69193168881119767227a663623abe40c62f4233
nbg <Gokul.Nb@rsa.com>
2021-09-21 13:35:21 +0530

    nested component addition. And style changes.

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,22 +1,22 @@
 import Controller from '@ember/controller';
 
 export default class CartController extends Controller {
-    // subtotal = 12;
-    // tax = 15;
-    // total = 0;
+  // subtotal = 12;
+  // tax = 15;
+  // total = 0;
 
-    get tax(){
-        return 0.09 * this.subtotal;
-    }
+  get tax() {
+    return 0.09 * this.subtotal;
+  }
 
-    get total(){
-        return this.subtotal + this.tax;
-    }
+  get total() {
+    return this.subtotal + this.tax;
+  }
 
-    get subtotal(){
-        return this.model.reduce((acc, item) => {
-            console.log("a=",acc, "b=", item);
-            return acc + item.price;
-        }, 0);
-    }
+  get subtotal() {
+    return this.model.reduce((acc, item) => {
+      console.log('a=', acc, 'b=', item);
+      return acc + item.price;
+    }, 0);
+  }
 }
