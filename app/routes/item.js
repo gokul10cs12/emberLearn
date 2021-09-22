47f5416b978cf5cef2e059981e0ff782df0c6fe8
gokulnb <gokul10cs12@gmail.com>
2021-09-23 01:05:42 +0530

    code update.

--- app/routes/item.js
+++ app/routes/item.js
@@ -1,8 +1,11 @@
 import Route from '@ember/routing/route';
-
+import {products} from '../data/products'
 export default class ItemRoute extends Route {
   model(param) {
     const { item_id } = param;
+
+    const productDetails = products.find( ({id}) => id === item_id ) ;
+    console.log(productDetails);
     return item_id;
-  }
+   }
 }
