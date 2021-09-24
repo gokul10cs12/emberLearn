c4da22e8814349174416fb24746cc42dbbc5de49
nbg <Gokul.Nb@rsa.com>
2021-09-24 19:10:58 +0530

    code refactoring.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,5 @@
 import Route from '@ember/routing/route';
-import { prod0ucts } from '../data/products';
+import { products } from '../data/products';
 export default class IndexRoute extends Route {
   model() {
     return products;
