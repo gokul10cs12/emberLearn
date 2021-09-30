0cfb1a52aa2658609b166d4bcd2cf7f1fb314be9
nbg <Gokul.Nb@rsa.com>
2021-09-30 19:12:30 +0530

    random data addition

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,9 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 export default class IndexRoute extends Route {
+  @service store;
   async model() {
-    const response = await fetch('/api/items.json');
-    const { data } = await response.json();
-    return data;
+    console.log("tths");
+    return this.store.findAll('products');
   }
 }
--- app/routes/item.js
+++ app/routes/item.js
@@ -1,13 +1,10 @@
 import Route from '@ember/routing/route';
-import {inejct as service} from '@ember/service';
 export default class ItemRoute extends Route {
-  @service store;
   async model(param) {
     const { item_id } = param;
 
-    const response = await fetch('/api/items.json');
-    const { data } = await response.json();
-
+    const { data } = this.store.findAll('product');
+    console.log(data);
     const productDetails = data.find(({ id }) => id === item_id);
     console.log(productDetails.colors);
     return productDetails;
